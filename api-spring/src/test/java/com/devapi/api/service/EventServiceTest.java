e1c23177c42feae74cbf5b45712171a3cbd92dea
Jonathan <jsa@id.uff.br>
2025-01-26 22:05:58 -0300

    Ajuste utilizando SonarQube

--- api-spring/src/test/java/com/devapi/api/service/EventServiceTest.java
+++ api-spring/src/test/java/com/devapi/api/service/EventServiceTest.java
@@ -8,14 +8,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Date;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class EventServiceTest {
+class EventServiceTest {
 
     private EventService eventService;
     private Event mockEvent;
